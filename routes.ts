8083d6c1ffd5d9848478d1b1cf195a40681c44bd
DustinBanks34 <148727181+DustinBanks34@users.noreply.github.com>
2025-02-04 17:01:29 -0500

    Add files via upload

---
+++ routes.ts
@@ -0,0 +1,41 @@
+// Import fetch module for making asynchronous requests
+import fetch from "node-fetch";
+
+// Create a route to existing endpoint & use a fat arrow function with, 
+// concise body syntax to return the string
+const routeHello = (): string => "Hello World!";
+
+// Create a route for a new endpoint, this endpoint will  add a page to our, 
+// web server displaying a list of usernames & IDs.
+const routeAPINames = async (): Promise<string> => {
+    const url = "https://www.usemodernfullstack.dev/api/v1/users";
+    let data: responseItemType[];
+    try {
+
+        // use await for fetch call
+        const response = await fetch(url);
+
+        // convert to json as soon as the call succeeds
+        data = (await response.json()) as responseItemType[];
+    } catch (err) {
+        return err;
+    }
+    const names = data
+    .map((item) => `id: ${item.id}, name: ${item.name}`)
+
+    // use break tags to display them in rows and return the string
+    .join("<br>");
+    return names;
+};
+const routeWeather = (query: WeatherQueryInterface): WeatherDetailType =>
+    queryWeatherData(query);
+
+const queryWeatherData = (query: WeatherQueryInterface): WeatherDetailType => {
+    return {
+        zipcode: query.zipcode,
+        weather: "sunny",
+        temp: 35
+    };
+};
+// export the two routes under their specified names
+export { routeHello, routeAPINames, routeWeather };
