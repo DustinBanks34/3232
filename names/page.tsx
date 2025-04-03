ee249d0786adcc351b91167dcc62ff964633d86c
DustinBanks34 <148727181+DustinBanks34@users.noreply.github.com>
2025-04-03 14:47:15 -0400

    Add files via upload

---
+++ names/page.tsx
@@ -0,0 +1,23 @@
+import { JSX } from "react";
+export default async function ServerComponentUserList(): Promise<JSX.Element> {
+    const url = "https://www.usemodernfullstack.dev/api/v1/users";
+    let data: ResponseType[] | [] = [];
+    let names : ResponseType[] | [];
+    try {
+        const response = await fetch(url, { cache: "force-cache"});
+        data = (await response.json()) as ResponseType[];
+    } catch (err) {
+        throw new Error("Failed to fetch data");
+    }
+    names = data.map((item) => {
+        return { id: item.id, name: item.name };
+    });
+
+    return (
+        <ul>
+            {names.map((item, index) => (
+                <li key="{item.id}">{index + 1}. {item.name}</li>
+            ))}
+        </ul>
+    );
+}
